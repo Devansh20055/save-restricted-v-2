16471f2b202492c15049ff56e7b4f855f5e070e2
DEVANSH <79394737+Devansh20055@users.noreply.github.com>
2022-02-09 15:10:00 +0530

    Update _start.py

--- main/plugins/_start.py
+++ main/plugins/_start.py
@@ -1,21 +1,39 @@
 #Github.com/Vasusen-code
 
 import os
-from .. import bot as Drone
-from telethon import events, Button
+from .. import bot
+from telethon import events, Button, TelegramClient
 
-from ethon.mystarts import start_srb
+from pyrogram import idle
+from main.plugins.main import Bot, userbot
+
+st = "--__ **HEY Buddy 🤡 Send me Link of message to clone it here\n 👻 FOR PUBLIC CHANNEL SEND DIRECT LINK 👻 \n ⚜️ For private channel message, Send invite link first ⚜️.\n\n 👩🏻‍💻👨🏻‍💻SUPPORT: @OFF_CHATS 👩🏻‍💻👨🏻‍💻\n🔆 POWERD BY :- @TEAM_SILENT_KING 🔆\n 🔰DEV: @ITS_NOT_ROMEO 🔰 ** __--"
+
+@bot.on(events.NewMessage(incoming=True, pattern="/start"))
+async def start(event):
+    await event.reply(f'{st}', 
+                      buttons=[
+                              [Button.inline("SET THUMB.", data="sett"),
+                               Button.inline("REM THUMB.", data="remt")]
+                              ])
+    try:
+        await Bot.start()
+        await userbot.start()
+        await idle()
+    except Exception as e:
+        if 'Client is already connected' in str(e):
+            pass
+        else:
+            return
     
-S = '/' + 's' + 't' + 'a' + 'r' + 't'
-
-@Drone.on(events.callbackquery.CallbackQuery(data="set"))
+@bot.on(events.callbackquery.CallbackQuery(data="sett"))
 async def sett(event):    
     Drone = event.client                    
     button = await event.get_message()
     msg = await button.get_reply_message() 
     await event.delete()
     async with Drone.conversation(event.chat_id) as conv: 
-        xx = await conv.send_message("Send me any image for thumbnail as a `reply` to this message.")
+        xx = await conv.send_message("Send me any image for thumbnail as a `reply` to this message. \n🔆 POWERD BY :- @TEAM_SILENT_KING 🔆")
         x = await conv.get_reply()
         if not x.media:
             xx.edit("No media found.")
@@ -23,16 +41,16 @@ async def sett(event):
         if not 'png' in mime:
             if not 'jpg' in mime:
                 if not 'jpeg' in mime:
-                    return await xx.edit("No image found.")
+                    return await xx.edit("No image found. ")
         await xx.delete()
         t = await event.client.send_message(event.chat_id, 'Trying.')
         path = await event.client.download_media(x.media)
         if os.path.exists(f'{event.sender_id}.jpg'):
             os.remove(f'{event.sender_id}.jpg')
         os.rename(path, f'./{event.sender_id}.jpg')
-        await t.edit("Temporary thumbnail saved!")
+        await t.edit("Temporary thumbnail saved! \n\n 🔆 POWERD BY :- @TEAM_SILENT_KING 🔆")
         
-@Drone.on(events.callbackquery.CallbackQuery(data="rem"))
+@bot.on(events.callbackquery.CallbackQuery(data="remt"))
 async def remt(event):  
     Drone = event.client            
     await event.edit('Trying.')
@@ -40,9 +58,6 @@ async def remt(event):
         os.remove(f'{event.sender_id}.jpg')
         await event.edit('Removed!')
     except Exception:
-        await event.edit("No thumbnail saved.")                        
-  
-@Drone.on(events.NewMessage(incoming=True, pattern=f"{S}"))
-async def start(event):
-    await start_srb(event)
+        await event.edit("No thumbnail saved.\n\n 🔆 POWERD BY :- @TEAM_SILENT_KING 🔆")                        
+    
     
