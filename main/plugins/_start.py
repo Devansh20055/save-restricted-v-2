50ec3c8ee835288164113a8d3e355e2913f36860
DEVANSH <79394737+Devansh20055@users.noreply.github.com>
2022-02-09 17:24:55 +0530

    Update _start.py

--- main/plugins/_start.py
+++ main/plugins/_start.py
@@ -53,5 +53,5 @@ async def start(event):
                       buttons=[
                               [Button.inline("SET THUMB.", data="sett"),
                                Button.inline("REM THUMB.", data="remt"),
-                                Button("DEVELPOERS .",URL="https://t.me/team_silent_king")]
+                                ]
                               ])
